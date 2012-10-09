c64206f3a8f7e8d6c4e8cd5bc13bca9b45b22838
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-10-09 14:03:41 -0500

    Removed remnants of jenna's email; fixed whitespace

--- chirp/volunteers/forms.py
+++ chirp/volunteers/forms.py
@@ -12,34 +12,34 @@ from django.contrib.sites.models import Site
 
 class ResetPasswordForm(forms.Form):
     """
-    A form that lets a volunteer user change reset his/her password 
+    A form that lets a volunteer user change reset his/her password
     based on email address.
     """
     email = forms.CharField(label=_("Email"), widget=forms.TextInput)
-    new_password1 = forms.CharField(label=_("New password"), 
+    new_password1 = forms.CharField(label=_("New password"),
                                             widget=forms.PasswordInput)
-    new_password2 = forms.CharField(label=_("New password confirmation"), 
+    new_password2 = forms.CharField(label=_("New password confirmation"),
                                             widget=forms.PasswordInput)
 
     def __init__(self, *args, **kwargs):
         # lookup user by email:
         # self.user = user
         super(ResetPasswordForm, self).__init__(*args, **kwargs)
-    
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         q = User.objects.filter(email=email)
         if q.count()==0:
             raise forms.ValidationError(_(
                 "No user exists with that email.  Please ask "
-                "jenna@chirpradio.org to add you to the volunteer tracker."))
+                "volunteers@chirpradio.org to add you to the volunteer tracker."))
         else:
             matching_users = q.all()
             if len(matching_users) > 1:
                 raise forms.ValidationError(_(
                     "Email belongs to multiple users.  Please ask "
-                    "jenna@chirpradio.org to reset your password manually."))
-            
+                    "volunteers@chirpradio.org to reset your password manually."))
+
             self.user = matching_users[0]
         return email
 
@@ -52,9 +52,9 @@ class ResetPasswordForm(forms.Form):
         return password2
 
     def save(self):
-        
+
         current_site = Site.objects.get_current()
-        
+
         transaction.enter_transaction_management()
         transaction.managed(True)
         try:
@@ -79,29 +79,29 @@ class ResetPasswordForm(forms.Form):
                 transaction.commit()
         finally:
             transaction.leave_transaction_management()
-        
+
         return self.user
-        
+
 class ManageTasksForm(forms.Form):
-    
+
     def __init__(self, *args, **kw):
         super(ManageTasksForm, self).__init__(*args, **kw)
-        
+
         choices = [(0, _("[any volunteer]"))] # first selection choice
         for v in Volunteer.objects.all().order_by('user__first_name'):
             choices.append((v.id, "%s %s" % (v.user.first_name, v.user.last_name)))
         self['volunteer'].field.choices = choices
-        
+
         choices = [(0, _("[any event]"))] # first selection choice
         for e in Event.objects.all().order_by('name', '-start_date'):
             choices.append((e.id, e))
         self['event'].field.choices = choices
-        
+
         choices = [(0, _("[status]"))]
         for s in TaskStatus.objects.all().order_by('status'):
             choices.append((s.id, s.status))
         self['set_status_for_all'].field.choices = choices
-        
+
     volunteer = forms.ChoiceField()
     event = forms.ChoiceField()
     set_status_for_all = forms.ChoiceField()
@@ -119,12 +119,12 @@ def next_month(curdatetime):
     return curdatetime.replace(day=1)
 
 class VolunteerActivityForm(forms.Form):
-    
+
     def __init__(self, *args, **kw):
         super(VolunteerActivityForm, self).__init__(*args, **kw)
-        
+
         choices = []
-        # make a range that starts a year ago, to give enough time 
+        # make a range that starts a year ago, to give enough time
         # to calculate past activity
         curdatetime = datetime.datetime.now() - timedelta(weeks=52)
         now = datetime.datetime.now()
@@ -132,16 +132,16 @@ class VolunteerActivityForm(forms.Form):
             curmonth = curdatetime.date()
             choices.append((curmonth.strftime("%Y-%m-01"), curmonth.strftime("%b 1st, %Y")))
             curdatetime = next_month(curdatetime)
-        
+
         self['as_of_date'].field.choices = choices
-        
+
     def clean_as_of_date(self):
         as_of_date = self.cleaned_data['as_of_date']
         # can only be a date like 2009-01-01
         return re.sub(r'[^0-9-]', '', as_of_date)
-        
+
     as_of_date = forms.ChoiceField()
-    
+
 
 class CloneEventForm(forms.Form):
     existing_event = forms.ChoiceField()
